f396f947a83d04103d423d94775c0f116bf24633
LuisMejia007 <paranoidandroidz180@gmail.com>
2018-11-16 00:55:56 -0800

    Initial process in developing backend Spring Boot code to work with our SQLite database

---
+++ comiclysm/src/main/java/com/comiclysm/project/comiclysm/ComiclysmApplication.java
@@ -0,0 +1,17 @@
+package com.comiclysm.project.comiclysm;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComiclysmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComiclysmApplication.class, args);
+
+	}
+
+
+}
